2a61ee66383d7bef9e60ac9fdcdc907fa7d092fe
AditiLatane <118658286+AditiLatane@users.noreply.github.com>
2024-01-11 15:44:57 +0530

    Create Guess.java

---
+++ Guess.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class First{
+    public static void main(String[] args){
+        int num = (int)(Math.random()*100);
+        //System.out.println(num);
+        System.out.println("Guess the number(1-100) : ");
+        System.out.println("Enter '-1' to stop");
+                    
+        do
+        {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            if (a<0){
+                System.out.println("Thank you ");
+                break;
+            }
+            else{
+                if(a==num){
+                    System.out.println("Correct");
+                    break;
+                }
+                else{
+                    if(a<num){
+                        System.out.println("Guess a greater number ");
+                    }
+                    else{
+                        System.out.println("Guess a smaller number ");
+                    
+                    }
+                }
+            }
+        }while (true);
+            
+        }
+}
